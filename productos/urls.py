0554ee02dd9d670ed3d80d13137f85c55ba52316
Cristian D Biancotti <cdbiancotti@gmail.com>
2024-12-10 09:01:57 -0300

    Clase 22 - Playground Avanzado Parte I

--- productos/urls.py
+++ productos/urls.py
@@ -4,5 +4,9 @@ from productos import views
 app_name = 'productos'
 
 urlpatterns = [
-    path('paletas/', views.VerPaleta.as_view(), name='listado_paletas')
+    path('paletas/', views.Paletas.as_view(), name='listado_paletas'),
+    path('paletas/crear/', views.CrearPaleta.as_view(), name='crear_paleta'),
+    path('paletas/<int:pk>/', views.VerPaleta.as_view(), name='ver_paleta'),
+    path('paletas/<int:pk>/editar/', views.EditarPaleta.as_view(), name='editar_paleta'),
+    path('paletas/<int:pk>/eliminar/', views.EliminarPaleta.as_view(), name='eliminar_paleta'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from productos.models import Paleta
+from django.urls import reverse_lazy
 
 
-class VerPaleta(ListView):
+class Paletas(ListView):
     model = Paleta
     template_name = "productos/listado_paletas.html"
     context_object_name = 'listado_paletas'
+
+class CrearPaleta(CreateView):
+    model = Paleta
+    template_name = "productos/crear_paleta.html"
+    fields = ['marca', 'anio', 'fecha']
+    success_url = reverse_lazy('productos:listado_paletas')
+
+class VerPaleta(DetailView):
+    model = Paleta
+    template_name = "productos/ver_paleta.html"
+
+class EditarPaleta(UpdateView):
+    model = Paleta
+    template_name = "productos/editar_paleta.html"
+    fields = ['marca', 'anio']
+    success_url = reverse_lazy('productos:listado_paletas')
+
+class EliminarPaleta(DeleteView):
+    model = Paleta
+    template_name = "productos/eliminar_paleta.html"
+    success_url = reverse_lazy('productos:listado_paletas')
